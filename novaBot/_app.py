bf3edd081722c0c083103ba8b0bc72cfd3f3e96b
Morgan Fell <morganfell@icloud.com>
2020-05-15 14:08:44 +0100

    Added temp_emote deletion after command use.

--- novaBot/_app.py
+++ novaBot/_app.py
@@ -31,11 +31,13 @@ async def jumbo(ctx, emoji: discord.PartialEmoji):
         request_file = requests.get(parsed_url)
         open("/var/www/html/resources/emotes_grabbed/temp_emote.gif", 'wb').write(request_file.content)
         await ctx.send(file=discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.gif"))
+        os.remove("/var/www/html/resources/emotes_grabbed/temp_emote.gif")
     else:
         parsed_url = "https://cdn.discordapp.com/emojis/" + emoji_url + ".png"
         request_file = requests.get(parsed_url)
         open("/var/www/html/resources/emotes_grabbed/temp_emote.png", 'wb').write(request_file.content)
         await ctx.send(file=discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.png"))
+        os.remove("/var/www/html/resources/emotes_grabbed/temp_emote.png")
 
 
 @bot.command()
