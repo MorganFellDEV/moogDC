c19353d6541d0713110f097708db2134049e2b9a
Morgan Fell <morganfell@icloud.com>
2020-05-15 17:02:08 +0100

    Forgot to remove the extra parts of the return statement.

--- novaBot/get_emote.py
+++ novaBot/get_emote.py
@@ -8,13 +8,13 @@ def get_emote_image(ctx, emoji: discord.PartialEmoji):
         parsed_url = "https://cdn.discordapp.com/emojis/" + str(emoji.id) + ".gif"
         request_file = requests.get(parsed_url)
         open("/var/www/html/resources/emotes_grabbed/temp_emote.gif", 'wb').write(request_file.content)
-        return discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.gif"), True
+        return discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.gif")
 
     else:
         parsed_url = "https://cdn.discordapp.com/emojis/" + str(emoji.id) + ".png"
         request_file = requests.get(parsed_url)
         open("/var/www/html/resources/emotes_grabbed/temp_emote.png", 'wb').write(request_file.content)
-        return discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.png"), False
+        return discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.png")
 
 
 def cleanup_emote(ctx, emoji: discord.PartialEmoji):
