eadfe71964fc0f88e525e4322351155ec1e35a2a
Morgan Fell <morganfell@icloud.com>
2020-05-15 16:43:46 +0100

    Fixed AttributeError on get_emote.py.
    Was calling "emoji.id" on a string, rather than the discord.PartialEmoji object.

--- novaBot/get_emote.py
+++ novaBot/get_emote.py
@@ -1,15 +1,17 @@
 import requests
 import os
 import discord
-def get_emote_image(ctx,emoji):
+
+
+def get_emote_image(ctx, emoji: discord.PartialEmoji):
     if emoji.animated:
-        parsed_url = "https://cdn.discordapp.com/emojis/" + emoji + ".gif"
+        parsed_url = "https://cdn.discordapp.com/emojis/" + emoji.id + ".gif"
         request_file = requests.get(parsed_url)
         open("/var/www/html/resources/emotes_grabbed/temp_emote.gif", 'wb').write(request_file.content)
         ctx.send(file=discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.gif"))
         os.remove("/var/www/html/resources/emotes_grabbed/temp_emote.gif")
     else:
-        parsed_url = "https://cdn.discordapp.com/emojis/" + emoji + ".png"
+        parsed_url = "https://cdn.discordapp.com/emojis/" + emoji.id + ".png"
         request_file = requests.get(parsed_url)
         open("/var/www/html/resources/emotes_grabbed/temp_emote.png", 'wb').write(request_file.content)
         ctx.send(file=discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.png"))
