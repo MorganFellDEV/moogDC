0dfea91b40411d607944eb03a3dcaebb1d5cbe54
Morgan Fell <morganfell@icloud.com>
2020-05-15 16:37:48 +0100

    Removed await from get_emote.py

--- novaBot/get_emote.py
+++ novaBot/get_emote.py
@@ -1,15 +1,16 @@
 import requests
+import os
 import discord
 def get_emote_image(ctx,emoji):
     if emoji.animated:
         parsed_url = "https://cdn.discordapp.com/emojis/" + emoji + ".gif"
         request_file = requests.get(parsed_url)
         open("/var/www/html/resources/emotes_grabbed/temp_emote.gif", 'wb').write(request_file.content)
-        await ctx.send(file=discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.gif"))
+        ctx.send(file=discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.gif"))
         os.remove("/var/www/html/resources/emotes_grabbed/temp_emote.gif")
     else:
         parsed_url = "https://cdn.discordapp.com/emojis/" + emoji + ".png"
         request_file = requests.get(parsed_url)
         open("/var/www/html/resources/emotes_grabbed/temp_emote.png", 'wb').write(request_file.content)
-        await ctx.send(file=discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.png"))
+        ctx.send(file=discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.png"))
         os.remove("/var/www/html/resources/emotes_grabbed/temp_emote.png")
