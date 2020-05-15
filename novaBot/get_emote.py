fe343543857c7afadedba7e7ff6904c393b37281
Morgan Fell <morganfell@icloud.com>
2020-05-15 16:46:03 +0100

    Fixed output of emoji.id to parsed_url.

--- novaBot/get_emote.py
+++ novaBot/get_emote.py
@@ -5,13 +5,13 @@ import discord
 
 def get_emote_image(ctx, emoji: discord.PartialEmoji):
     if emoji.animated:
-        parsed_url = "https://cdn.discordapp.com/emojis/" + emoji.id + ".gif"
+        parsed_url = "https://cdn.discordapp.com/emojis/" + str(emoji.id) + ".gif"
         request_file = requests.get(parsed_url)
         open("/var/www/html/resources/emotes_grabbed/temp_emote.gif", 'wb').write(request_file.content)
         ctx.send(file=discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.gif"))
         os.remove("/var/www/html/resources/emotes_grabbed/temp_emote.gif")
     else:
-        parsed_url = "https://cdn.discordapp.com/emojis/" + emoji.id + ".png"
+        parsed_url = "https://cdn.discordapp.com/emojis/" + str(emoji.id) + ".png"
         request_file = requests.get(parsed_url)
         open("/var/www/html/resources/emotes_grabbed/temp_emote.png", 'wb').write(request_file.content)
         ctx.send(file=discord.File("/var/www/html/resources/emotes_grabbed/temp_emote.png"))
